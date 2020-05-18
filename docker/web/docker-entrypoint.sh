08902262e5714b1095474a00320deffa8dd45fdf
dmitriy <korionov.dmytro@onix-systems.com>
2020-05-19 00:17:50 +0300

    fix psotgresql

--- docker/web/docker-entrypoint.sh
+++ docker/web/docker-entrypoint.sh
@@ -43,5 +43,4 @@ fi
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-#gunicorn django_project.wsgi -b 0.0.0.0:${BIND_PORT:-8000}
-python manage.py runserver 0.0.0.0:8000
+gunicorn django_project.wsgi -b 0.0.0.0:${BIND_PORT:-8000}
